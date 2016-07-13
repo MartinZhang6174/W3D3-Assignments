0c774ceb00d739d9ddfadafa685670cce6e0c60a
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-07-13 11:08:39 -0700

    Project created

---
+++ InstaKilo/InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Martin Zhang on 2016-07-13.
+//  Copyright © 2016 Martin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Martin Zhang on 2016-07-13.
+//  Copyright © 2016 Martin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
